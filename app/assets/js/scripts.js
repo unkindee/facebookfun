94c8c0e506e3b286ceab09c33eb36432352737e1
dragos <dragosbucatica@gmail.com>
2018-10-29 18:18:26 +0200

    facebook project

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * template
+ * Dragos test
+ * https://dragosisme.com
+ * @author Dragos
+ * @version 1.0.0
+ * Copyright 2018. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    //
+  });
+
+})(jQuery, window, document);
